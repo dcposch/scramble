3344d732b599d644ec65beba79dcdf1b182dba80
Jae Kwon <jkwon.work@gmail.com>
2013-09-17 17:24:42 -0700

    added migration script to migrate old data on scramble.io

--- migrations.go
+++ migrations.go
@@ -99,17 +99,60 @@ func migratePasswordHash() error {
 
 func migrateEmailRefactor() error {
 
-	// TODO: migration of existing data
+	// Migration of existing data
+	// Load everything onto memory, wipe table, then reinsert.
+	type OldEmailRow struct {
+		// Columns from the old email table
+		UnixTime int64
+		MessageID, From, To, CipherSubject, CipherBody, Box string
+		PubHashTo, PubHashFrom string
 
-	_, err := db.Exec(`ALTER TABLE email
-        DROP INDEX pub_hash_to,
-        DROP INDEX pub_hash_from,
-        DROP pub_hash_from,
-        DROP pub_hash_to,
-        DROP primary key,
-		MODIFY to_email TEXT NOT NULL,
-        ADD PRIMARY KEY (message_id)`)
+		// Convenience. Aggregate {<pub_hash> -> [<box1>,<box2>]} here
+		Boxes map[string][]string
+	}
+	rows, err := db.Query(`SELECT
+        m.unix_time, m.message_id, m.from_email, m.to_email,
+        m.cipher_subject, m.cipher_body, m.box,
+        m.pub_hash_to, m.pub_hash_from
+		FROM email as m`)
+	if err != nil { return err }
+	oldEmails := map[string]*OldEmailRow{}
+	for rows.Next() {
+		oldEmail := &OldEmailRow{}
+		err = rows.Scan(
+			&oldEmail.UnixTime,
+			&oldEmail.MessageID,
+			&oldEmail.From,
+			&oldEmail.To,
+			&oldEmail.CipherSubject,
+			&oldEmail.CipherBody,
+			&oldEmail.Box,
+			&oldEmail.PubHashTo,
+			&oldEmail.PubHashFrom,
+		)
+		if err != nil { return err }
+		if oldEmails[oldEmail.MessageID] == nil {
+			oldEmails[oldEmail.MessageID] = oldEmail
+			oldEmail.Boxes = map[string][]string{}
+		}
+		boxesForRecipient := oldEmails[oldEmail.MessageID].Boxes[oldEmail.PubHashTo]
+		if boxesForRecipient == nil {
+			oldEmails[oldEmail.MessageID].Boxes[oldEmail.PubHashTo] = []string{oldEmail.Box}
+		} else {
+			boxesForRecipient = append(boxesForRecipient, oldEmail.Box)
+		}
+	}
 
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS new_email (
+        message_id     CHAR(40) NOT NULL,
+        unix_time      BIGINT NOT NULL,
+        from_email     VARCHAR(254) NOT NULL,
+        to_email       TEXT NOT NULL,
+        cipher_subject VARCHAR(1500) NOT NULL,
+        cipher_body    LONGTEXT NOT NULL,
+
+        primary key (message_id)
+    )`)
 	if err != nil { return err }
 
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS box (
@@ -123,6 +166,61 @@ func migrateEmailRefactor() error {
         INDEX (address, box, unix_time),
         INDEX (address, message_id)
     )`)
+	if err != nil { return err }
+
+	// Reinsert everything from oldEmails
+	for _, oldEmail := range oldEmails {
+		// insert row into email
+		_, err := db.Exec(`INSERT INTO new_email
+            (message_id, unix_time, from_email, to_email, cipher_subject, cipher_body)
+            VALUES (?,?,?,?,?,?)`,
+			oldEmail.MessageID,
+			oldEmail.UnixTime,
+			oldEmail.From,
+			oldEmail.To,
+			oldEmail.CipherSubject,
+			oldEmail.CipherBody,
+		)
+		if err != nil { return err }
+		// insert row(s) into box
+		var sentToSelf bool = false
+		for pubHashTo, boxes := range oldEmail.Boxes {
+			addressTo := pubHashTo+"@scramble.io"
+			if oldEmail.PubHashFrom == pubHashTo && len(boxes) == 1 && boxes[0] != "sent" {
+				sentToSelf = true
+			}
+			for _, box := range boxes {
+				_, err := db.Exec(`INSERT INTO box
+                    (message_id, address, box, unix_time)
+                    VALUES (?,?,?,?)`,
+					oldEmail.MessageID,
+					addressTo,
+					box,
+					oldEmail.UnixTime,
+				)
+				if err != nil { return err }
+			}
+		}
+		// if sender also sent to self, there was no
+		// corresponding "sent" entry, so create one.
+		if sentToSelf {
+			_, err := db.Exec(`INSERT INTO box
+				(message_id, address, box, unix_time)
+				VALUES (?,?,?,?)`,
+				oldEmail.MessageID,
+				oldEmail.From,
+				"sent",
+				oldEmail.UnixTime,
+			)
+			if err != nil { return err }
+		}
+	}
+
+	_, err = db.Exec(`DROP TABLE email`)
+	if err != nil { return err }
+
+	_, err = db.Exec(`RENAME TABLE new_email to email`)
+	if err != nil { return err }
 
 	return err
 }
